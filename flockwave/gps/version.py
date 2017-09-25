de62808ef16c7d75f996ea5559323c8be3540852
Tamas Nepusz <ntamas@gmail.com>
2017-09-25 19:08:29 +0200

    bumped version number to 0.7.2

--- flockwave/gps/version.py
+++ flockwave/gps/version.py
@@ -1,4 +1,4 @@
 """Version information for the Flockwave GPS package."""
 
-__version_info__ = (0, 7, 1)
+__version_info__ = (0, 7, 2)
 __version__ = ".".join("{0}".format(i) for i in __version_info__)
