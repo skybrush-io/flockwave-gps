d8a9d45982087266556ee080db4e3f7601da12a7
Tamas Nepusz <ntamas@gmail.com>
2020-10-26 16:24:51 +0100

    feat: added functions related to conversion between datetime and GPS time

---
+++ src/flockwave/gps/time.py
@@ -0,0 +1,125 @@
+"""GPS time related utility functions.
+
+The functions in this module should provide the same results as the GPS-to-UTC
+converter at https://www.gw-openscience.org/gps/
+"""
+
+from bisect import bisect
+from datetime import datetime, timedelta, timezone
+from typing import Optional, Tuple
+
+__all__ = (
+    "current_gps_week",
+    "datetime_to_gps_time",
+    "datetime_to_gps_time_of_week",
+    "gps_time_to_utc",
+    "gps_time_of_week_to_utc",
+)
+
+
+#: Epoch of GPS time
+GPS_EPOCH = datetime(1980, 1, 6, 0, 0, 0)
+
+#: Dates when leap seconds have occurred since the GPS epoch (1980-01-01)
+#: Consult https://www.timeanddate.com/time/leap-seconds-future.html to keep this
+#: up-to-date
+_LEAP_DATES = (
+    (1981, 6, 30),
+    (1982, 6, 30),
+    (1983, 6, 30),
+    (1985, 6, 30),
+    (1987, 12, 31),
+    (1989, 12, 31),
+    (1990, 12, 31),
+    (1992, 6, 30),
+    (1993, 6, 30),
+    (1994, 6, 30),
+    (1995, 12, 31),
+    (1997, 6, 30),
+    (1998, 12, 31),
+    (2005, 12, 31),
+    (2008, 12, 31),
+    (2012, 6, 30),
+    (2015, 6, 30),
+    (2016, 12, 31),
+)
+
+#: Datetime objects representing the occurrences of leap seconds since the GPS epoch
+LEAP_DATES = tuple(datetime(i[0], i[1], i[2], 23, 59, 59) for i in _LEAP_DATES)
+
+#: Number of seconds in a week in GPS time
+SECONDS_IN_WEEK = 604800
+
+
+def current_gps_week() -> int:
+    """Returns the current GPS week number."""
+    return datetime_to_gps_time_of_week(datetime.now(timezone.utc))[0]
+
+
+def leap_seconds_since_1980(date: datetime) -> int:
+    """Returns the number of leap seconds since 1980-01-01
+
+    Parameters:
+        date: the time we are interested in, in UTC
+
+    Returns:
+        leap seconds up to the given time
+    """
+    if date.tzinfo is not None:
+        date = date.replace(tzinfo=None)
+    return bisect(LEAP_DATES, date)
+
+
+def gps_time_to_utc(timestamp: int) -> datetime:
+    """Converts a GPS timestamp expressed as the number of seconds since the GPS
+    epoch into a timezone-aware UTC datetime object.
+
+    Parameters:
+        seconds: the number of sceonds since the beginning of the week
+        week: the GPS week number; `None` means to use the current GPS week
+    """
+    date_before_leaps = GPS_EPOCH + timedelta(seconds=timestamp)
+    result = date_before_leaps - timedelta(
+        seconds=leap_seconds_since_1980(date_before_leaps)
+    )
+    return result.replace(tzinfo=timezone.utc)
+
+
+def gps_time_of_week_to_utc(
+    seconds: Optional[float] = 0, *, week: Optional[int] = None
+) -> datetime:
+    """Converts a GPS timestamp expressed as the number of seconds since the
+    beginning of the current (GPS) week and the GPS week number into a
+    timezone-aware UTC datetime object.
+
+    Parameters:
+        seconds: the number of sceonds since the beginning of the week
+        week: the GPS week number; `None` means to use the current GPS week
+    """
+    if week is None:
+        week = current_gps_week()
+    return gps_time_to_utc(week * SECONDS_IN_WEEK + seconds)
+
+
+def datetime_to_gps_time(dt: datetime) -> Tuple[int, int]:
+    """Converts a timezone-aware datetime object into a GPS timestamp, expressed
+    as seconds since the GPS epoch."""
+    if dt.tzinfo is None:
+        raise ValueError("not applicable to naive datetime objects")
+
+    date_naive = dt.astimezone(timezone.utc).replace(tzinfo=None)
+    date_before_leaps = date_naive + timedelta(
+        seconds=leap_seconds_since_1980(date_naive)
+    )
+
+    return (date_before_leaps - GPS_EPOCH).total_seconds()
+
+
+def datetime_to_gps_time_of_week(dt: datetime) -> Tuple[int, int]:
+    """Converts a timezone-aware datetime object into GPS time, expressed
+    as the GPS week number and the number of seconds since the beginning of that
+    GPS week.
+    """
+    seconds = datetime_to_gps_time(dt)
+    week, seconds = divmod(seconds, SECONDS_IN_WEEK)
+    return int(week), seconds
---
+++ test/gps/test_time.py
@@ -0,0 +1,47 @@
+from datetime import datetime, timezone
+from flockwave.gps.time import (
+    current_gps_week,
+    datetime_to_gps_time_of_week,
+    gps_time_of_week_to_utc,
+    leap_seconds_since_1980,
+)
+
+
+def d(value: str) -> datetime:
+    utc = timezone.utc
+    if " " in value:
+        return datetime.strptime(value, "%Y-%m-%d %H:%M:%S").replace(tzinfo=utc)
+    else:
+        return datetime.strptime(value, "%Y-%m-%d").replace(tzinfo=utc)
+
+
+def f(date: datetime) -> str:
+    return date.strftime("%Y-%m-%d %H:%M:%S")
+
+
+def test_current_gps_week():
+    # smoke testing only
+    assert current_gps_week() >= 2129
+    assert isinstance(current_gps_week(), int)
+
+
+def test_leap_seconds_since_1980():
+    assert leap_seconds_since_1980(d("1979-01-01")) == 0
+    assert leap_seconds_since_1980(d("1979-12-31")) == 0
+    assert leap_seconds_since_1980(d("1996-12-31")) == 11
+    assert leap_seconds_since_1980(d("1997-06-30")) == 11
+    assert leap_seconds_since_1980(d("1997-07-01")) == 12
+    assert leap_seconds_since_1980(d("2017-04-03")) == 18
+    assert leap_seconds_since_1980(d("2020-12-26")) == 18
+
+
+def test_datetime_to_gps_time_of_week():
+    # authoritative source: https://www.gw-openscience.org/gps/
+    assert datetime_to_gps_time_of_week(d("2020-10-26 14:28:01")) == (2129, 138499)
+
+
+def test_gps_time_of_week_to_utc():
+    # authoritative source: https://www.gw-openscience.org/gps/
+    dt = gps_time_of_week_to_utc(138499, week=2129)
+    assert f(dt) == "2020-10-26 14:28:01"
+    assert dt.tzinfo is not None and dt.utcoffset().total_seconds() == 0
