3970dbaff4349942a42023f6e8ad43ab255d8490
Tamas Nepusz <ntamas@gmail.com>
2023-09-17 12:40:00 +0200

    chore: bumped version to 3.0.0

--- src/flockwave/gps/version.py
+++ src/flockwave/gps/version.py
@@ -1,4 +1,4 @@
 """Version information for the Flockwave GPS package."""
 
-__version_info__ = (2, 3, 3)
+__version_info__ = (3, 0, 0)
 __version__ = ".".join("{0}".format(i) for i in __version_info__)
